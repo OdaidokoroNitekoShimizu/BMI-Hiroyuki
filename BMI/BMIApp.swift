584f5bb2272b0072d842f7ee0f4c183fd77c7caf
Hiroyuki <lahm0107@gmail.com>
2025-11-02 17:11:24 +0900

    Initial Commit

---
+++ BMI/BMIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMIApp.swift
+//  BMI
+//
+//  Created by hiroyuki takahashi on R 7/11/02.
+//
+
+import SwiftUI
+
+@main
+struct BMIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
