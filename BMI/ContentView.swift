3fbc852250a30793cf8d85cb6c6d4f013a81885a
Hiroyuki <lahm0107@gmail.com>
2025-11-04 22:25:55 +0900

    1104課題完成

--- BMI/ContentView.swift
+++ BMI/ContentView.swift
@@ -8,10 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var Sintyou: Double = 0.0
+    @State private var Taizyuu: Double = 0.0
+    @State private var Tanni: String = "meter"
+    let Tannis = ["meter","centimeter"]
+    @FocusState private var isFocused: Bool
     var body: some View {
-        Form {
+        NavigationStack {
+            Form {
+                Section("入力"){
+                    HStack {
+                        TextField("身長", value: $Sintyou, format: .number)
+                            .keyboardType(.decimalPad)
+                            .padding()
+                            .focused($isFocused)
 
+
+                        Picker("", selection: $Tanni) {
+                            ForEach(Tannis, id: \.self) { Tanni in
+                                Text(Tanni)
+                            }
+                        }
+                        .pickerStyle(.menu)
+                    }
+
+                    HStack {
+
+                        TextField("体重", value: $Taizyuu, format: .number)
+                            .keyboardType(.decimalPad)
+                            .padding()
+                            .focused($isFocused)
+
+
+                        Text("kg")
+                    }
+
+                }
+                Section("結果"){
+                    if Tanni == "centimeter" {
+                        let bmi = Taizyuu / (Sintyou / 100) / (Sintyou / 100)
+                        Text(bmi,format: .number)
+                            .padding()
+                    }
+
+                    if Tanni == "meter" {
+                        let bmi = Taizyuu / Sintyou / Sintyou
+                        Text(bmi,format: .number)
+                            .padding()
+                    }
+                }
+
+                Button("キーボードを閉じる") {
+                    isFocused = false
+                }
+            }
+            .navigationTitle("BMI計算")
         }
+
     }
 }
 
